1ec5130125af3ca1467c8d05ffe9c13473f580f9
Ubuntu <ubuntu@ip-172-31-44-63.eu-central-1.compute.internal>
2018-01-25 15:20:04 +0000

    hist_euqalize

--- code/yumin/utils/resize.py
+++ code/yumin/utils/resize.py
@@ -3,21 +3,22 @@ import scipy.misc
 import os
 import pandas as pd
 from tqdm import tqdm
-
+from skimage import exposure
 
 def main():
-    csvTe_name = '/Users/yuminsun/dl4cvproject/data/test.csv'
-    test = pd.read_csv(csvTe_name)
-    prefixTe = '/Users/yuminsun/dl4cvproject/data/test_'
-    prefixTe256 = '/Users/yuminsun/dl4cvproject/data/test256'
-    #csvTr_name = '/Users/yuminsun/dl4cvproject/data/train.csv'
-    #train = pd.read_csv(csvTr_name)
-    #prefixTr = '/Users/yuminsun/dl4cvproject/data/train_'
-    #prefixTr256 = '/Users/yuminsun/dl4cvproject/data/train256'
-    for img_path in tqdm(test['image_name'].values):
-        fullname = os.path.join(prefixTe, img_path)
-        new_img = scipy.misc.imresize(imageio.imread(fullname), (256, 256))
-        scipy.misc.imsave(os.path.join(prefixTe256, img_path), new_img)
+	csvTe_name = '/home/ubuntu/dl4cvproject/data/test.csv'
+	test = pd.read_csv(csvTe_name)
+	prefixTe = '/home/ubuntu/dl4cvproject/data/test_'
+	prefixTe256 = '/home/ubuntu/dl4cvproject/data/test256'
+	#csvTr_name = '/home/ubuntu/dl4cvproject/data/train.csv'
+	#train = pd.read_csv(csvTr_name)
+	#prefixTr = '/home/ubuntu/dl4cvproject/data/train_'
+	#prefixTr256 = '/home/ubuntu/dl4cvproject/data/train256'
+	for img_path in tqdm(test['image_name'].values):
+		fullname = os.path.join(prefixTe, img_path)
+		new_img = scipy.misc.imresize(imageio.imread(fullname), (256, 256))
+		hist_img = exposure.equalize_hist(new_img)
+		scipy.misc.imsave(os.path.join(prefixTe256, img_path), hist_img)
 
 if __name__ == '__main__':
     main()
