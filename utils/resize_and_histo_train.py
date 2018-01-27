89011387be4dbb85de0f381b1dd9ca595cbb6b55
Yumin Sun <ga45vab@mytum.de>
2018-01-27 21:29:45 +0100

    remove not used python files, fix semantic error in ___train.py

--- utils/resize_and_histo_train.py
+++ utils/resize_and_histo_train.py
@@ -8,15 +8,15 @@ from skimage import exposure
 print('imported')
 
 def main():
-	csvTe_name = '~/dl4cvproject/data/train.csv'
-	test = pd.read_csv(csvTe_name)
-	prefixTe = '~/dl4cvproject/data/train_'
-	prefixTe256 = '~/dl4cvproject/data/train256'
-	for img_path in tqdm(test['image_name'].values):
-		fullname = os.path.join(os.path.expanduser(prefixTe), img_path)
+	csvTr_name = '~/dl4cvproject/data/train.csv'
+	train = pd.read_csv(csvTr_name)
+	prefixTr = '~/dl4cvproject/data/train_'
+	prefixTr256 = '~/dl4cvproject/data/train256'
+	for img_path in tqdm(train['image_name'].values):
+		fullname = os.path.join(os.path.expanduser(prefixTr), img_path)
 		new_img = scipy.misc.imresize(imageio.imread(fullname), (256, 256))
 		hist_img = exposure.equalize_hist(new_img)
-		scipy.misc.imsave(os.path.join(os.path.expanduser(prefixTe256), img_path), hist_img)
+		scipy.misc.imsave(os.path.join(os.path.expanduser(prefixTr256), img_path), hist_img)
 
 if __name__ == '__main__':
     main()
